64b9375dbe31f3962d714e60ae3045b131c63ee4
Herlon Aguiar <me@herlon.dev>
2022-12-09 23:50:08 +0100

    day 9 completed

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,379 +1,170 @@
-use std::{collections::HashSet, thread, time::Duration};
+use std::collections::HashSet;
 
-type Point = (usize, usize);
-
-trait Parser {
-    fn parse(input: &str) -> Self;
-}
-
-trait Follow {
-    fn calc_move(&self, target: Point) -> Point;
-    fn is_tailing(&self, target: Point) -> bool;
-}
-
-impl Follow for Point {
-    fn is_tailing(&self, target: Point) -> bool {
-        let horizontal_distance = self.0.abs_diff(target.0);
-        let vertical_distance = self.1.abs_diff(target.1);
-
-        match (horizontal_distance, vertical_distance) {
-            (0, 0) => true, // overlapping
-            (0, 1) => true,
-            (1, 0) => true,
-            (1, 1) => true,
-            _ => false,
-        }
-    }
-
-    fn calc_move(&self, target: Point) -> Point {
-        let head = (target.0 as i32, target.1 as i32);
-        let mut tail = (self.0 as i32, self.1 as i32);
-        let diff: (i32, i32) = (tail.0 - head.0, tail.1 - head.1);
-
-        println!("\t\tTHE DIFF IS {:?} => tail {:?}", diff, tail);
-
-        match diff {
-            // x
-            // 0.....T
-            // 1......
-            // 2.....H
-            //  543210 <- y
-            //
-            // T (50, 200) -> (0, 0)
-            // H (50, 202) -> (0, 2)
-            // (0, 1)
-            (2, 0) => {
-                tail.0 -= 1;
-            }
-            (-2, 0) => {
-                tail.0 += 1;
-            }
-            (0, 2) => {
-                tail.1 -= 1;
-            }
-            (0, -2) => {
-                tail.1 += 1;
-            }
-
-            // x
-            // 0...T..
-            // 1.....H
-            // 2......
-            //  543210 <- y
-            //
-            // T (50, 203) -> (2, 0)
-            // H (48, 204) -> (0, 1)
-            // (-1, +1)
-            (2, -1) => {
-                tail.0 -= 1;
-                tail.1 += 1;
-            }
-
-            // x
-            // 0.....H
-            // 1......
-            // 2....T.
-            //  543210 <- y
-            //
-            // H (46, 202) -> (0, 0)
-            // T (47, 204) -> (1, 2)
-            // (-1, -1)
-            (1, 2) => {
-                tail.0 -= 1;
-                tail.1 -= 1;
-            }
-
-            // x
-            // 0.....T
-            // 1......
-            // 2....H.
-            //  543210 <- y
-            //
-            // H (47, 204) -> (1, 2)
-            // T (46, 202) -> (0, 0)
-            // (1, 1)
-            (-1, -2) => {
-                tail.0 += 1;
-                tail.1 += 1;
-            }
-
-            // x
-            // 0....H.
-            // 1......
-            // 2.....T
-            //  543210 <- y
-            //
-            // H (48, 202) -> (1, 0)
-            // T (47, 204) -> (0, 2)
-            // (1,-1)
-            (-1, 2) => {
-                tail.0 += 1;
-                tail.1 -= 1;
-            }
-
-            // x
-            // 0.....T
-            // 1...H..
-            // 2......
-            //  543210 <- y
-            //
-            // H (56, 202) -> (2, 1)
-            // T (54, 201) -> (0, 0)
-            // (+1,+1)
-            (-2, -1) => {
-                tail.0 += 1;
-                tail.1 += 1;
-            }
-
-            // x
-            // 0....T.
-            // 1......
-            // 2.....H
-            //  543210 <- y
-            //
-            // H (57, 200) -> (2, 0)
-            // T (55, 201) -> (0, 1)
-            // (+1,-1)
-            (-2, 1) => {
-                tail.0 += 1;
-                tail.1 -= 1;
-            }
-
-            // x
-            // 0.....H
-            // 1...T..
-            // 2......
-            //  543210 <- y
-            //
-            // H (54, 197) -> (0, 0)
-            // T (56, 198) -> (2, 1)
-            // (-1,-1)
-            (2, 1) => {
-                tail.0 -= 1;
-                tail.1 -= 1;
-            }
-
-            // x
-            // 0....T.
-            // 1......
-            // 2.....H
-            //  543210 <- y
-            //
-            // H (53, 198) -> (0, 2)
-            // T (54, 196) -> (1, 0)
-            // (-1,1)
-            (1, -2) => {
-                tail.0 -= 1;
-                tail.1 += 1;
-            }
-
-            // x
-            // 0...H..
-            // 1......
-            // 2.....T
-            //  543210 <- y
-            //
-            // H (302, 298) -> (2, 0)
-            // T (300, 300) -> (0, 2)
-            // (-1,1)
-            (-2, 2) => {
-                tail.0 -= 1;
-                tail.1 += 1;
-            }
-
-            // x
-            // 0...H..
-            // 1......
-            // 2.....T
-            //  543210 <- y
-            //
-            // H (300, 302) -> (0, 2)
-            // T (302, 300) -> (2, 0)
-            // (-1,1)
-            (2, -2) => {
-                tail.0 -= 1;
-                tail.1 += 1;
-            }
-
-            _ => unreachable!("Diff {:?} with head={:?} and tail={:?}", diff, head, tail),
-        }
-
-        (tail.0 as usize, tail.1 as usize)
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Debug)]
 enum Direction {
     Up,
     Down,
-    Right,
     Left,
+    Right,
 }
 
 impl Direction {
     fn as_tuple(&self) -> (i32, i32) {
         match self {
-            Direction::Up => (-1, 0),
-            Direction::Down => (1, 0),
-            Direction::Right => (0, 1),
-            Direction::Left => (0, -1),
+            Direction::Up => (0, 1),
+            Direction::Down => (0, -1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
         }
     }
 }
 
-type Movement = (Direction, usize);
-impl Parser for Movement {
-    fn parse(input: &str) -> Movement {
-        let parts: Vec<&str> = input.split(' ').collect();
-        let direction = match *parts.get(0).unwrap() {
+impl From<&str> for Direction {
+    fn from(input: &str) -> Self {
+        match input {
             "R" => Direction::Right,
             "L" => Direction::Left,
             "U" => Direction::Up,
             "D" => Direction::Down,
             _ => unreachable!(),
-        };
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Movement(Direction, usize);
+
+impl From<&str> for Movement {
+    fn from(input: &str) -> Self {
+        let parts: Vec<&str> = input.split(' ').collect();
+        let direction: Direction = parts.get(0).unwrap().to_owned().into();
         let amount = parts.get(1).unwrap().parse::<usize>().unwrap();
 
-        (direction, amount)
+        Movement(direction, amount)
     }
 }
 
-struct Grid {
-    grid: Vec<Vec<char>>,
-    head: Point,
-    knots: Vec<Point>,
-    tail_counter: HashSet<Point>,
-}
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Point(i32, i32);
+impl Point {
+    fn move_to(&mut self, direction: &Direction) {
+        let tuple = direction.as_tuple();
 
-impl Grid {
-    // Generate a MxM grid
-    fn new(size: usize, start: Point, knots_number: usize) -> Self {
-        let mut grid: Vec<Vec<char>> = vec![];
+        self.0 += tuple.0;
+        self.1 += tuple.1;
+    }
 
-        for _ in 0..size {
-            let col: Vec<char> = vec!['.'; size];
-            grid.push(col);
-        }
+    fn is_tailed_to(&self, target: &Point) -> bool {
+        let diff_x = target.0 - self.0;
+        let diff_y = target.1 - self.1;
 
-        // Tag start
-        grid[start.0][start.1] = 's';
-
-        Grid {
-            grid,
-            head: start,
-            knots: vec![start; knots_number],
-            tail_counter: HashSet::new(),
+        match (diff_x, diff_y) {
+            // Same
+            (0, 0) => true,
+            // Right
+            (1, 0) => true,
+            // Left
+            (-1, 0) => true,
+            // Top
+            (0, 1) => true,
+            // Down
+            (0, -1) => true,
+            // Top left
+            (-1, 1) => true,
+            // Bottom left
+            (-1, -1) => true,
+            // Top right
+            (1, 1) => true,
+            // Bottom right
+            (1, -1) => true,
+            _ => false,
         }
     }
 
-    fn tag_position(&mut self, pos: Point, identifier: char) {
-        self.grid[pos.0][pos.1] = identifier
-    }
+    fn follow(&mut self, target: &Point) {
+        let diff_x = target.0 - self.0;
+        let diff_y = target.1 - self.1;
 
-    fn move_knots(&mut self) {
-        let mut prev = self.head;
-
-        let knots: Vec<Point> = self
-            .knots
-            .iter()
-            .enumerate()
-            .map(|(idx, knot)| {
-                if knot.is_tailing(prev) {
-                    // println!("\t[TAIL] {:?} is tailing {:?}", &knot, &prev);
-
-                    return *knot;
-                }
-
-                let new_position = prev.calc_move(*knot);
-
-                println!("\t[TAIL] Moving from {:?} to {:?}", &knot, new_position);
-
-                prev = new_position;
-
-                if idx == &self.knots.len() - 1 {
-                    self.tail_counter.insert(new_position.clone());
-                }
-
-                new_position
-            })
-            .collect();
-
-        for (i, knot) in knots.iter().rev().enumerate() {
-            self.tag_position(*knot, char::from_digit(i as u32, 10).unwrap());
+        if self.is_tailed_to(target) {
+            return;
         }
 
-        // Tag last knot
-        // let last = knots.last().unwrap();
-        // if self.grid[last.0][last.1] != '#' {
-        //     self.tag_position(*last, '#');
-        //     self.tail_counter += 1;
-        // }
-
-        // Update
-        self.knots = knots;
-    }
-
-    fn walk(&mut self, movement: Movement) {
-        let calc = movement.0.as_tuple();
-
-        if self.grid[self.head.0][self.head.1] == 'H' {
-            self.grid[self.head.0][self.head.1] = '.';
-        }
-
-        let mut new_position = (self.head.0 as i32, self.head.1 as i32);
-
-        new_position.0 += calc.0;
-        new_position.1 += calc.1;
-
-        println!(
-            "[HEAD] Moving {:?} from {:?} to {:?}",
-            movement.0, &self.head, new_position
-        );
-
-        self.head = (new_position.0 as usize, new_position.1 as usize);
-
-        self.tag_position(self.head, 'H');
-
-        self.move_knots();
-    }
-
-    fn print(&self) {
-        let mut output = "".to_string();
-
-        for i in 0..self.grid.len() {
-            let mut line: Vec<String> = vec![];
-            for j in 0..self.grid.len() {
-                line.push(self.grid[i][j].to_string());
+        match (diff_x, diff_y) {
+            (2, 0) => self.move_to(&Direction::Right),
+            (-2, 0) => self.move_to(&Direction::Left),
+            (-2, n) if n > 0 => {
+                self.move_to(&Direction::Up);
+                self.move_to(&Direction::Left);
+            }
+            (2, n) if n > 0 => {
+                self.move_to(&Direction::Up);
+                self.move_to(&Direction::Right);
+            }
+            (2, n) if n < 0 => {
+                self.move_to(&Direction::Down);
+                self.move_to(&Direction::Right);
+            }
+            (-2, n) if n < 0 => {
+                self.move_to(&Direction::Down);
+                self.move_to(&Direction::Left);
+            }
+            (0, 2) => self.move_to(&Direction::Up),
+            (0, -2) => self.move_to(&Direction::Down),
+            (1, 2) => {
+                self.move_to(&Direction::Right);
+                self.move_to(&Direction::Up);
+            }
+            (1, -2) => {
+                self.move_to(&Direction::Right);
+                self.move_to(&Direction::Down);
+            }
+            (-1, 2) => {
+                self.move_to(&Direction::Left);
+                self.move_to(&Direction::Up);
+            }
+            (-1, -2) => {
+                self.move_to(&Direction::Left);
+                self.move_to(&Direction::Down);
             }
 
-            output = format!("{}\n{}", output, line.join(""));
+            _ => unreachable!("Diff x {} diff y {}", diff_x, diff_y),
         }
-
-        std::fs::write("output", output).expect("Unable to write file");
     }
+}
+
+fn read_input(input: &str, knots_number: usize) -> usize {
+    let mut head = Point(0, 0);
+    let mut knots = vec![Point(0, 0); knots_number];
+
+    let mut pos: HashSet<Point> = HashSet::new();
+
+    for line in input.lines() {
+        let movement: Movement = line.into();
+        for _ in 0..movement.1 {
+            head.move_to(&movement.0);
+
+            let mut prev = head.clone();
+            for i in 0..knots.len() {
+                knots[i].follow(&prev);
+                prev = knots[i].clone();
+            }
+
+            // Store the position of the last knot
+            pos.insert(prev.clone());
+        }
+    }
+
+    pos.len()
 }
 
 fn main() {
     let input = include_str!("../input");
 
-    let mut grid = Grid::new(900, (300, 300), 5);
+    let visited = read_input(input, 1);
+    println!("Total visited with 1 knot: {}", visited);
 
-    let movements = input
-        .lines()
-        .map(|line| Movement::parse(line))
-        .flat_map(|(direction, count)| (0..count).map(move |_| (direction.clone(), 1)))
-        .collect::<Vec<_>>();
-
-    for movement in movements {
-        grid.print();
-        grid.walk(movement);
-    }
-
-    grid.tag_position((300, 300), 's');
-    grid.tag_position(grid.head, 'H');
-    grid.tag_position(grid.knots[0], 'T');
-    grid.print();
-    println!("Tail visited {} positions", grid.tail_counter.len());
+    let visited = read_input(input, 9);
+    println!("Total visited with 9 knots: {}", visited);
 }
 
 #[cfg(test)]
@@ -381,24 +172,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn small_grid() {
-        let grid = Grid::new(10, (0, 0), 2);
-
-        assert_eq!(grid.grid.len(), 10);
-        assert_eq!(grid.grid[0].len(), 10);
-        assert_eq!(grid.grid[0][5], '.');
-    }
-
-    #[test]
-    fn parse_movement_str() {
-        assert_eq!(Movement::parse("R 4"), (Direction::Right, 4));
-        assert_eq!(Movement::parse("L 10"), (Direction::Left, 10));
-        assert_eq!(Movement::parse("D 40"), (Direction::Down, 40));
-        assert_eq!(Movement::parse("U 5"), (Direction::Up, 5));
-    }
-
-    fn example() {
-        let mut grid = Grid::new(7, (5, 1), 0);
+    fn example_1() {
         let input = r"R 4
 U 4
 L 3
@@ -408,45 +182,7 @@ D 1
 L 5
 R 2";
 
-        for line in input.lines() {
-            grid.walk(Movement::parse(line));
-        }
-
-        grid.print();
-
-        assert_eq!(grid.grid[1][3], '#');
-        assert_eq!(grid.grid[1][4], '#');
-        assert_eq!(grid.grid[2][5], '#');
-        assert_eq!(grid.grid[3][5], '#');
-        assert_eq!(grid.grid[3][4], '#');
-        assert_eq!(grid.grid[3][3], '#');
-        assert_eq!(grid.grid[4][5], '#');
-        assert_eq!(grid.grid[5][4], '#');
-        assert_eq!(grid.grid[5][3], '#');
-        assert_eq!(grid.grid[5][2], '#');
-        assert_eq!(grid.grid[5][1], '#');
-
-        assert_eq!(grid.tail_counter, 13);
-    }
-
-    #[test]
-    fn part_2() {
-        let mut grid = Grid::new(100, (50, 50), 9);
-        let input = r"R 5
-U 8
-L 8
-D 3
-R 17
-D 10
-L 25
-U 20";
-
-        for line in input.lines() {
-            grid.print();
-            grid.walk(Movement::parse(line));
-        }
-
-        grid.print();
-        panic!("asdasd");
+        let visited = read_input(input, 1);
+        assert_eq!(visited, 13);
     }
 }
